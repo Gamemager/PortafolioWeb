edcfd1d6586515ed55426d630524942d38eb81ed
Eixon <eixond@gmail.com>
2026-03-02 22:59:37 -0500

    actualizacion de paginas

--- src/app/components/bento-grid/bento-grid.ts
+++ src/app/components/bento-grid/bento-grid.ts
@@ -12,31 +12,39 @@ export class BentoGrid {
   techStack = ['ANGULAR', 'TAILWIND', 'BOOTSTRAP', 'NODE.JS', 'FIGMA', 'CSS3'];
 
   projects = [
-    {
-      title: 'Enterprise CRM',
-      desc: 'Gestión de clientes con dashboard administrativo complejo.',
-      tags: ['Angular', 'Node.js', 'CRUD'],
-      img: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2340&auto=format&fit=crop'
-    },
+    // {
+    //   title: 'Enterprise CRM',
+    //   desc: 'Gestión de clientes con dashboard administrativo complejo.',
+    //   tags: ['Angular', 'Node.js', 'CRUD'],
+    //   img: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2340&auto=format&fit=crop'
+    // },
     {
       title: 'E-Commerce Platform',
       desc: 'Tienda online completa con pasarela de pagos.',
       tags: ['Angular', 'Bootstrap', 'Mercado Pago'],
-      img: 'https://images.unsplash.com/photo-1472851294608-062f824d29cc?q=80&w=2304&auto=format&fit=crop'
+      img: 'https://images.unsplash.com/photo-1472851294608-062f824d29cc?q=80&w=2304&auto=format&fit=crop',
+      link: 'https://krayola.netlify.app/'
     },
     {
-      title: 'SaaS Landing Page',
+      title: 'Landing Page',
       desc: 'Página de alta conversión optimizada para SEO.',
       tags: ['Tailwind', 'Figma', 'SEO'],
-      img: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2426&auto=format&fit=crop'
+      img: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2426&auto=format&fit=crop',
+      link: 'https://eixonportafolio.netlify.app/'
     },
     {
       title: 'System Dashboard',
       desc: 'Panel de control analítico con reportes en tiempo real.',
-      tags: ['Angular', 'ChartJS'],
-      img: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2340&auto=format&fit=crop'
+      tags: ['Angular' ],
+      img: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2340&auto=format&fit=crop',
+      link: ''
     }
   ];
+    openProject(url: string) {
+    if (url && url !== '#') {
+      window.open(url, '_blank');
+    }
+  }
    scrollToProjects() {
     const element = document.getElementById('projects');
     if (element) {
